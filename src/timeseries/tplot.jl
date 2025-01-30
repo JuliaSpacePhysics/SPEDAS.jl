0e013885de0f877e654f75c9fd27f98728138175
Beforerr <zzj956959688@gmail.com>
2025-01-30 10:33:41 -0800

    fix: tplot

--- src/timeseries/tplot.jl
+++ src/timeseries/tplot.jl
@@ -7,21 +7,31 @@ struct FigureAxes
     axes::AbstractArray{<:Axis}
 end
 
+struct AxisPlots
+    axis::Axis
+    plots
+end
+
 Makie.convert_arguments(P::Type{<:Lines}, ta::TimeArray) = convert_arguments(P, timestamp(ta), values(ta))
 
 
 ylabel(ta) = ""
 
 function ylabel(tas::AbstractVector{<:TimeArray})
-    op(x, y) = x == y ? x : "$x, $y"
+    op(x, y) = ((x != y) && y != "") ? "$x, $y" : x
     mapreduce(ylabel, op, tas)
+end
+
+function prioritized_get(c, keys::AbstractVector, default)
+    values = get.(Ref(c), keys, nothing)
+    something(values..., default)
 end
 
 function ylabel(ta::TimeArray)
     m = meta(ta)
     m === nothing && return ""
-    label = get(m, "label", "")
-    unit = get(m, "unit", "")
+    label = prioritized_get(m, ["label", "long_name"], "")
+    unit = prioritized_get(m, ["unit", "units"], "")
     isempty(unit) ? label : "$label ($unit)"
 end
 
@@ -29,18 +39,17 @@ end
 Overlay multiple columns of a time series on the same axis
 """
 function tplot!(ax::Axis, ta::TimeArray; kwargs...)
-    for p in propertynames(ta)
+    map(propertynames(ta)) do p
         lines!(ax, getproperty(ta, p); label=string(p))
     end
 end
 
-"""
-Overlay multiple columns of a time series on the same axis
-"""
-function tplot!(ax::Axis, ta::AbstractDimArray; labeldim=nothing, kwargs...)
-    args, attributes = _series(ta, kwargs, labeldim)
-    series!(ax, args...; labels=attributes.labels, kwargs...)
+function tplot(gp::GridPosition, ta::TimeArray; kwargs...)
+    ax = Axis(gp, ylabel=ylabel(ta))
+    plots = tplot!(ax, ta; kwargs...)
+    AxisPlots(ax, plots)
 end
+
 
 """
     tplot!(ax, tas; kwargs...)
@@ -53,21 +62,37 @@ function tplot!(ax::Axis, tas::AbstractVector; kwargs...)
     end
 end
 
+
+"""
+Setup the axis on a position and plot multiple time series on it
+"""
 function tplot(gp::GridPosition, tas::AbstractVector; kwargs...)
-    ax = Axis(gp, ylabel=ylabel(ta))
+    ax = Axis(gp, ylabel=ylabel(tas))
     plots = map(tas) do ta
         tplot!(ax, ta; kwargs...)
     end
-    ax, plots
+    AxisPlots(ax, plots)
 end
 
+"""
+Overlay multiple columns of a time series on the same axis
+"""
+function tplot!(ax::Axis, ta::AbstractDimArray; labeldim=nothing, kwargs...)
+    args, attributes = _series(ta, kwargs, labeldim)
+    series!(ax, args...; labels=attributes.labels, kwargs...)
+end
+
+"""
+Plot a multivariate time series on a position in a figure
+"""
 function tplot(gp::GridPosition, ta::AbstractDimArray; labeldim=nothing, kwargs...)
     args, attributes = _series(ta, kwargs, labeldim)
     series(gp, args...; attributes...)
 end
 
+
 """
-Lay out multiple time series on the same figure
+Lay out multiple time series on the same figure across different panels (rows)
 """
 function tplot(f, tas::AbstractVector; add_legend=true, link_xaxes=true, kwargs...)
     aps = map(enumerate(tas)) do (i, ta)
@@ -85,7 +110,7 @@ end
 function tplot(tas; figure=(;), kwargs...)
     f = Figure(; figure...)
     fa = tplot(f, tas; kwargs...)
-    fa
+    f
 end
 
 
