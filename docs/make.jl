0bdcfddaa300e2f25efcad312f35f74c084f2d66
Beforerr <zzj956959688@gmail.com>
2025-01-31 08:59:45 -0800

    CI: deploy

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,15 @@ using Documenter
 using SpaceTools
 
 makedocs(
-    sitename = "SpaceTools",
-    format = Documenter.HTML(),
-    modules = [SpaceTools]
+    sitename="SpaceTools",
+    format=Documenter.HTML(),
+    modules=[SpaceTools],
+    doctest=true
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo="github.com/Beforerr/SpaceTools.jl",
+)
