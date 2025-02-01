3ac6b977054a1438027963bf08f2836cc6e5af08
Beforerr <zzj956959688@gmail.com>
2025-02-01 09:07:51 -0800

    feat: DimensionData utils

--- src/utils.jl
+++ src/utils.jl
@@ -9,3 +9,12 @@ function prioritized_get(c, keys::AbstractVector, default)
     values = get.(Ref(c), keys, nothing)
     something(values..., default)
 end
+
+function modify_meta(da; kwargs...)
+    new_meta = merge(da.metadata, kwargs)
+    rebuild(da; metadata=new_meta)
+end
+
+function Base.rename(da::AbstractDimArray, new_name)
+    rebuild(da; name=new_name)
+end
