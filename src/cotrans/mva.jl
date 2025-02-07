d0a3fbd9871b74a9d4db433ea54e8571ac06df2b
Beforerr <zzj956959688@gmail.com>
2025-02-06 22:30:02 -0800

    feat: error estimate for MVA

--- src/cotrans/mva.jl
+++ src/cotrans/mva.jl
@@ -35,7 +35,10 @@ function mva_mat(Bx, By, Bz; check=false)
 end
 
 mva_mat(B::AbstractMatrix; kwargs...) = mva_mat(eachcol(B)...; kwargs...)
-mva_mat(B::AbstractMatrix{Q}; kwargs...) where {Q<:Quantity} = mva_mat(ustrip(B); kwargs...)
+function mva_mat(B::AbstractMatrix{Q}; kwargs...) where {Q<:Quantity}
+    F = mva_mat(ustrip(B); kwargs...)
+    Eigen(F.values * unit(Q)^2, F.vectors)
+end
 
 """
     mva(V::AbstractMatrix, B::AbstractMatrix; kwargs...)
@@ -91,3 +94,44 @@ function is_right_handed(F::Eigen)
     v3 = vs[:, 3]
     is_right_handed(v1, v2, v3)
 end
+
+################
+# Error Estimate
+################
+
+"""
+    Δφij(λᵢ, λⱼ, λ₃, M)
+
+Calculate the phase error between components i and j according to:
+|Δφᵢⱼ| = |Δφⱼᵢ| = √(λ₃/(M-1) * (λᵢ + λⱼ - λ₃)/(λᵢ - λⱼ)²)
+
+Parameters:
+- λᵢ: eigenvalue i
+- λⱼ: eigenvalue j
+- λ₃: smallest eigenvalue (λ₃)
+- M: number of samples
+"""
+function Δφij(λᵢ, λⱼ, λ₃, M)
+    return sqrt((λ₃ / (M - 1)) * (λᵢ + λⱼ - λ₃) / (λᵢ - λⱼ)^2)
+end
+
+"""
+Calculate the composite statistical error estimate for ⟨B·x₃⟩:
+|Δ⟨B·x₃⟩| = √(λ₃/(M-1) + (Δφ₃₂⟨B⟩·x₂)² + (Δφ₃₁⟨B⟩·x₁)²)
+
+Parameters:
+- λ₁, λ₂, λ₃: eigenvalues in descending order
+- M: number of samples
+- B: mean magnetic field vector
+- x₁, x₂, x₃: eigenvectors
+"""
+function B_x3_error(λ₁, λ₂, λ₃, M, B, x₁, x₂, x₃)
+    Δφ₃₂ = Δφij(λ₃, λ₂, λ₃, M)
+    Δφ₃₁ = Δφij(λ₃, λ₁, λ₃, M)
+    B_x₂ = dot(B, x₂)
+    B_x₁ = dot(B, x₁)
+    return sqrt(λ₃ / (M - 1) + (Δφ₃₂ * B_x₂)^2 + (Δφ₃₁ * B_x₁)^2)
+end
+
+B_x3_error(F::Eigen, M, B) =
+    B_x3_error(F.values..., M, B, eachcol(F.vectors)...)
