2e4ffdf8a801abadc7aeafbee8f3718d567d44e9
Beforerr <zzj956959688@gmail.com>
2025-02-07 23:39:46 -0800

    perf: Remove the resolution-based updates and only update based on axis limit to improve performance

--- src/timeseries/interactive.jl
+++ src/timeseries/interactive.jl
@@ -1,13 +1,16 @@
-using InteractiveViz: Continuous1D, iviz
+using InteractiveViz: Continuous1D, iviz, FigureAxisPlotEx
+
+abstract type AbstractRangeFunction end
 
 ### 1D range function that takes a range as input and returns an array
-struct RangeFunction1D{F,L} <: InteractiveViz.Continuous1D
+# struct RangeFunction1D{F,L} <: InteractiveViz.Continuous1D
+struct RangeFunction1D{F,L} <: AbstractRangeFunction
     f::F
     xmin::L
     xmax::L
 end
 
-function InteractiveViz.sample(data::RangeFunction1D, xrange::AbstractRange, yrange; samples=10000)
+function sample(data::RangeFunction1D, xrange::AbstractRange, yrange; samples=10000)
     xmin = first(xrange)
     xmax = last(xrange)
     x, y = data.f((xmin, xmax))
@@ -23,7 +26,42 @@ function InteractiveViz.sample(data::RangeFunction1D, xrange::AbstractRange, yra
     (; x, y)
 end
 
-InteractiveViz.limits(data::RangeFunction1D) = (data.xmin, data.xmax, nothing, nothing)
+limits(data::RangeFunction1D) = (data.xmin, data.xmax, nothing, nothing)
+
+"""
+Remove the resolution-based updates and only update based on axis limit to improve performance
+"""
+function InteractiveViz.iviz(f, data::RangeFunction1D; delay=0.1)
+    lims = limits(data)
+    r = range(lims[1], lims[2]; length=2)
+    qdata = sample(data, r, nothing)
+    x = Observable(qdata.x)
+    y = Observable(qdata.y)
+    fap = f(x, y)
+
+    if current_axis().limits[] == (nothing, nothing)
+        xlims!(current_axis(), lims[1], lims[2])
+    end
+
+    reset_limits!(current_axis())
+
+    function update(lims)
+        xrange = range(lims.origin[1], lims.origin[1] + lims.widths[1])
+        yrange = range(lims.origin[2], lims.origin[2] + lims.widths[2])
+        qdata = sample(data, xrange, yrange)
+        x.val = qdata.x
+        return y[] = qdata.y
+    end
+    axislimits = current_axis().finallimits
+    on(axislimits) do axlimits
+        if @isdefined(redraw_limit)
+            close(redraw_limit)
+        end
+        redraw_limit = Timer(x -> update(axlimits), delay)
+    end
+
+    return FigureAxisPlotEx(fap, () -> update(axislimits[]), nothing)
+end
 
 struct RangeFunction2D{F,L} <: InteractiveViz.Continuous2D
     f::F
