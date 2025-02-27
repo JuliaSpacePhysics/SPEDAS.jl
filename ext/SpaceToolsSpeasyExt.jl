4332e3e447d96381cc761ccc565c7a6bfc074c01
Beforerr <zzj956959688@gmail.com>
2025-02-27 08:41:46 -0800

    refactor: SpaceToolsSpeasyExt

--- ext/SpaceToolsSpeasyExt.jl
+++ ext/SpaceToolsSpeasyExt.jl
@@ -1,25 +1,20 @@
 module SpaceToolsSpeasyExt
 
 using SpaceTools
-using SpaceTools: SpeasyProduct, DEFAULTS
+using SpaceTools: SpeasyProduct
 using Speasy
 using DimensionalData
-import SpaceTools: xlabel, ylabel
+import SpaceTools: transform
 
-SpaceTools.get_data(p::SpeasyVariable; kwargs...) = DimArray(p; kwargs...)
+SpaceTools.transform(p::SpeasyVariable; kwargs...) = DimArray(p; kwargs...)
 
 function SpaceTools.get_data(p::SpeasyProduct, args...; kwargs...)
     DimArray(get_data(p.id, args...; kwargs...))
 end
 
-SpaceTools.get_data(p::AbstractString, args...; kwargs...) = DimArray(get_data(p, args...; kwargs...))
-
-SpaceTools.meta(ta::SpeasyVariable) = ta.meta
-SpaceTools.ylabel(ta::SpeasyVariable) = ta.meta["LABLAXIS"]
-
-function SpaceTools.axis_attributes(sps::AbstractVector{SpeasyProduct}, tmin, tmax; add_title=DEFAULTS.add_title, kwargs...)
-    tas = SpaceTools.get_data.(sps, tmin, tmax; kwargs...)
-    SpaceTools.axis_attributes(tas; add_title, kwargs...)
+function SpaceTools.axis_attributes(sps::AbstractVector{SpeasyProduct}, tmin, tmax; kwargs...)
+    tas = SpaceTools.get_data.(sps, tmin, tmax)
+    SpaceTools.axis_attributes(tas; kwargs...)
 end
 
 end
