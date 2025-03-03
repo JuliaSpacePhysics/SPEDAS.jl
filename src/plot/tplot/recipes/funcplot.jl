6945b75347ca7cdd1050adf9c78396e48c421325
Beforerr <zzj956959688@gmail.com>
2025-03-03 09:07:12 -0800

    perf: reuse returned data

--- src/plot/tplot/recipes/funcplot.jl
+++ src/plot/tplot/recipes/funcplot.jl
@@ -11,11 +11,11 @@ Interactively plot a function over a time range on a grid position
 """
 function functionplot(gp, f, tmin, tmax; axis=(;), add_title=DEFAULTS.add_title, add_colorbar=DEFAULTS.add_colorbar, xtickformat=format_datetime, kwargs...)
     # get a sample data to determine the attributes and plot types
-    ta = f(tmin, tmax)
-    attrs = axis_attributes(ta; add_title, xtickformat=values -> xtickformat.(x2t.(values)))
+    data = f(tmin, tmax)
+    attrs = axis_attributes(data; add_title, xtickformat=values -> xtickformat.(x2t.(values)))
     ax = Axis(gp; attrs..., axis...)
-    plot = functionplot!(ax, f, tmin, tmax; kwargs...)
-    isspectrogram(ta) && add_colorbar && Colorbar(gp[1, 1, Right()], plot; label=clabel(ta))
+    plot = functionplot!(ax, f, tmin, tmax; data, kwargs...)
+    isspectrogram(data) && add_colorbar && Colorbar(gp[1, 1, Right()], plot; label=clabel(data))
     PanelAxesPlots(gp, AxisPlots(ax, plot))
 end
 
@@ -24,24 +24,24 @@ end
 
 Interactive plot of a function `f` on `ax` for a time range from `tmin` to `tmax`
 """
-function functionplot!(ax, f, tmin, tmax; kwargs...)
+function functionplot!(ax, f, tmin, tmax; data=nothing, kwargs...)
     # get a sample data to determine the attributes and plot types
-    ta = f(tmin, tmax)
-    attrs = plottype_attributes(ta)
+    data = @something data f(tmin, tmax)
+    attrs = plottype_attributes(data)
 
     # Manually converting from time to float is needed for interactive plotting since ax.finallimits[] is represented as float
     # https://github.com/MakieOrg/Makie.jl/issues/4769
     xmin, xmax = t2x.((tmin, tmax))
-    data = RangeFunction1D(time2value_transform(f), xmin, xmax)
-    plot_func = if isspectrogram(ta)
-        y = spectrogram_y_values(ta)
+    rf = RangeFunction1D(time2value_transform(f), xmin, xmax)
+    plot_func = if isspectrogram(data)
+        y = spectrogram_y_values(data)
         (x, mat) -> heatmap!(ax, x, y, mat; attrs..., kwargs...)
     else
-        if ndims(ta) == 2
+        if ndims(data) == 2
             (xs, vs) -> series!(ax, xs, @lift(permutedims($vs)); attrs..., kwargs...)
         else
             (xs, vs) -> lines!(ax, xs, vs; attrs..., kwargs...)
         end
     end
-    iviz(plot_func, data)
+    iviz(plot_func, rf)
 end
