eedab704a0bb9b6cd93a0d764c2fab4b61c461e2
Beforerr <zzj956959688@gmail.com>
2025-03-07 17:51:36 -0800

    feat: update resample functions to use default resample value from DEFAULTS

--- src/resampling/resample.jl
+++ src/resampling/resample.jl
@@ -1,10 +1,10 @@
 """
-    resample(arr, n=10000; dim=1, verbose=false)
+    resample(arr, n=DEFAULTS.resample; dim=1, verbose=false)
 
 Resample an array along the dimension `dim` to `n` points.
 If the original length is less than or equal to `n`, the original array is returned unchanged.
 """
-function resample(arr, n=10000; dim=1, verbose=false)
+function resample(arr, n=DEFAULTS.resample; dim=1, verbose=false)
     sz = size(arr, dim)
     if sz > n
         verbose && @info "Resampling array of size $(size(arr)) along dimension $dim from $sz to $n points"
@@ -16,12 +16,12 @@ function resample(arr, n=10000; dim=1, verbose=false)
 end
 
 """
-    tresample(da::DimArray, n=10000; dimtype=Ti)
+    tresample(da::DimArray, n=DEFAULTS.resample; dimtype=Ti)
 
 Resample a DimArray specifically along its dimension of type `dimtype` to `n` points.
 Throws an error if no dimension of type `dimtype` is found in the array.
 """
-function tresample(da::DimArray, n=10000; dimtype=Ti)
+function tresample(da::DimArray, n=DEFAULTS.resample; dimtype=Ti)
     time_dim = findfirst(d -> d isa dimtype, dims(da))
     isnothing(time_dim) && throw(ArgumentError("No dimension of type $dimtype found in the input DimArray"))
     resample(da, n; dim=time_dim)
--- src/types.jl
+++ src/types.jl
@@ -5,6 +5,7 @@ import Base: ∘
     add_title::Bool
     add_colorbar::Bool
     delay::Float64
+    resample::Int
 end
 
 """
@@ -15,11 +16,13 @@ A global constant that holds default parameters:
 - `add_title::Bool` defaults to `false`.
 - `add_colorbar::Bool` defaults to `true`.
 - `delay` : in seconds, the time interval between updates. Default is 0.25.
+- `resample::Int` : the number of points to resample to. Default is 6070.
 """
 const DEFAULTS = Defaults(;
     add_title=false,
     add_colorbar=true,
-    delay=0.25
+    delay=0.25,
+    resample=6070
 )
 
 abstract type AbstractProduct end
