a2085677857b3b7b729d4c8536ebd6b0da909eb6
Beforerr <zzj956959688@gmail.com>
2025-04-12 08:14:13 -0700

    fix: current_density timerange

--- src/analysis/current_density.jl
+++ src/analysis/current_density.jl
@@ -8,13 +8,13 @@ Calculate the current density time series from the magnetic field (B) and plasma
 Assume 1-D structure along the z-direction. Remember to transform the coordinates of B and V first (e.g. using [`mva`](@ref)
 """
 @views function current_density(B, V)
-    _B_in = tview(B, timerange(V))
+    _B_in, _V_in = tviews(B, V)
     ts = times(_B_in)
 
     dBdt = tderiv(parent(_B_in), ts)
     dBxdt = dBdt[:, 1]
     dBydt = dBdt[:, 2]
-    Vz = tinterp(V[:, 3], ts[1:end-1])
+    Vz = tinterp(_V_in[:, 3], ts[1:end-1])
 
     Jx = @. upreferred(dBydt / (μ0 * Vz))
     Jy = @. upreferred(-dBxdt / (μ0 * Vz))
