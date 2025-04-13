68913e861eab465a056d831b9d320331b89a6dfc
Beforerr <zzj956959688@gmail.com>
2025-04-13 12:02:50 -0700

    refactor: use SpaceDataModel and delete old and duplicate files

--- src/DataModel/methods.jl
+++
@@ -1,9 +0,0 @@
-"""
-    abbr(p::Project; lowercase = true)
-
-Get the abbreviation (abbr) of a project.
-"""
-function abbr(p::Project; lowercase=true)
-    ab = p.metadata["abbreviation"]
-    return lowercase ? Base.lowercase(ab) : ab
-end
