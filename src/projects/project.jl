3b5894179c1917ed94c3025cb1515a13f3cdb750
Beforerr <zzj956959688@gmail.com>
2025-04-23 12:48:25 -0700

    revert: add generated doc back

--- src/projects/project.jl
+++ src/projects/project.jl
@@ -25,7 +25,8 @@ function _load_project_config(file; mod=Main, directory=joinpath(pkgdir(@__MODUL
     @eval mod CONFIG = $config
     for (sym, value) in config
         # @eval mod const $sym = $value
-        @eval mod $sym = $value
+        doc = repr2doc(value)
+        @eval mod @doc $doc $sym = $value
         export_symbol && @eval mod export $sym
     end
 end
