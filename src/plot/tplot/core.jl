d3ecb86d18bb57c0ed814fbb02608dd8d109cee6
Beforerr <zzj956959688@gmail.com>
2025-04-30 20:46:33 -0700

    feat: mappable

--- src/plot/tplot/core.jl
+++ src/plot/tplot/core.jl
@@ -3,7 +3,11 @@ Core functionality for time series plotting.
 This file contains the main `tplot` function and its variants.
 """
 
-default_palette(x) = ((i, 1) for i in 1:length(x))
+default_palette(x) = ((i, 1) for i in eachindex(x))
+
+mappable(x) = values(x)
+mappable(x::SupportTypes) = (x,)
+mappable(x::Product) = (x,)
 
 """
     tplot(f, tas; legend=(; position=Right()), link_xaxes=true, link_yaxes=false, rowgap=5, transform=transform_pipeline, kwargs...)
@@ -16,11 +20,11 @@ By default, the time series are transformed via `transform_pipeline`, which is e
 See also: [`tplot_panel`](@ref), [`transform_pipeline`](@ref), [`transform`](@ref)
 """
 function tplot(f::Drawable, tas, args...; legend=(; position=Right()), link_xaxes=true, link_yaxes=false, rowgap=5, transform=transform_pipeline, axis=(;), palette=default_palette(tas), kwargs...)
-    gaps = map(palette, values(tas)) do pos, ta
+    gaps = map(palette, mappable(tas)) do pos, ta
         gp = f[pos...]
         pap = tplot_panel(gp, ta, args...; transform, axis, kwargs...)
         # Hide redundant x labels
-        link_xaxes && pos[1] != length(tas) && hidexdecorations!.(pap.axis, grid=false)
+        link_xaxes && pos != last(palette) && hidexdecorations!.(pap.axis, grid=false)
         pap
     end
 
@@ -34,9 +38,8 @@ function tplot(f::Drawable, tas, args...; legend=(; position=Right()), link_xaxe
     FigureAxes(f, axs)
 end
 
-tplot(f::Drawable, ta::SupportTypes, args...; kwargs...) = tplot(f, (ta,), args...; kwargs...)
 function tplot(ta, args...; figure=(;), kwargs...)
-    tas = ta isa SupportTypes ? (ta,) : ta
+    tas = mappable(ta)
     f = Figure(; size=auto_figure_size(tas), figure...)
     tplot(f, tas, args...; kwargs...)
 end
