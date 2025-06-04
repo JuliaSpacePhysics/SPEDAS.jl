bc266eb7ea5cab4c1329429133dd62c0346c2e10
Zijin Zhang <58776897+Beforerr@users.noreply.github.com>
2025-06-04 09:10:55 -0700

    feat: add support for array time views in tplot (#44)

--- src/plot/tplot/recipes/funcplot.jl
+++ src/plot/tplot/recipes/funcplot.jl
@@ -2,13 +2,15 @@
     plotfunc = tplot_panel!
 end
 
+apply(f, args...) = f(args...)
+apply(A::AbstractArray, tmin, tmax) = tview(A, tmin, tmax)
 
 """
     functionplot(gp, f, tmin, tmax; kwargs...)
 
 Interactively plot a function over a time range on a grid position
 """
-function functionplot(gp, f, tmin, tmax; axis=(;), add_title=DEFAULTS.add_title, add_colorbar=DEFAULTS.add_colorbar, plot=(;), kwargs...)
+function functionplot(gp, f, tmin, tmax; axis = (;), add_title = DEFAULTS.add_title, add_colorbar = DEFAULTS.add_colorbar, plot = (;), kwargs...)
     # get a sample data to determine the attributes and plot types
     data = f(tmin, tmax)
     m = @something meta(f) Dict()
@@ -19,8 +21,8 @@ function functionplot(gp, f, tmin, tmax; axis=(;), add_title=DEFAULTS.add_title,
     ax = Axis(gp; attrs..., axis...)
     plot = _merge(plottype_attributes(m), plot)
     p = functionplot!(ax, f, tmin, tmax; data, plot, kwargs...)
-    isspectrogram(data) && add_colorbar && Colorbar(gp[1, 1, Right()], p; label=clabel(data))
-    PanelAxesPlots(gp, AxisPlots(ax, p))
+    isspectrogram(data) && add_colorbar && Colorbar(gp[1, 1, Right()], p; label = clabel(data))
+    return PanelAxesPlots(gp, AxisPlots(ax, p))
 end
 
 """
@@ -28,9 +30,9 @@ end
 
 Interactive plot of a function `f` on `ax` for a time range from `tmin` to `tmax`
 """
-function functionplot!(ax, f, tmin, tmax; data=nothing, plotfunc=plot2spec, plot=(;), kwargs...)
+function functionplot!(ax, f, tmin, tmax; data = nothing, plotfunc = plot2spec, plot = (;), kwargs...)
     to_plotspec = trange -> plotfunc(f(trange...); plot...)
-    iviz_api!(ax, to_plotspec, (tmin, tmax); kwargs...)
+    return iviz_api!(ax, to_plotspec, (tmin, tmax); kwargs...)
 end
 
 """
@@ -39,10 +41,11 @@ end
 Specialized multiplot function for `functionplot`.
 Merge specs before plotting so as to cycle through them.
 """
-function multiplot!(ax::Axis, fs, tmin, tmax; plotfunc=plot2spec, plot=(;), kwargs...)
+function multiplot!(ax::Axis, fs, tmin, tmax; plotfunc = plot2spec, plot = (;), kwargs...)
     to_plotspec = trange -> mapreduce(vcat, fs) do f
         attrs = plottype_attributes(meta(f))
-        plotfunc(f(trange...); attrs..., plot...)
+        x = apply(f, trange...)
+        plotfunc(x; attrs..., plot...)
     end
-    iviz_api!(ax, to_plotspec, (tmin, tmax); kwargs...)
+    return iviz_api!(ax, to_plotspec, (tmin, tmax); kwargs...)
 end
--- src/plot/tplot/recipes/specplot.jl
+++ src/plot/tplot/recipes/specplot.jl
@@ -6,24 +6,27 @@
 
 Plot a spectrogram on a panel
 """
-function specplot(gp, ta; axis=(;), add_colorbar=DEFAULTS.add_colorbar, add_title=DEFAULTS.add_title, kwargs...)
+function specplot(gp, ta; axis = (;), add_colorbar = DEFAULTS.add_colorbar, add_title = DEFAULTS.add_title, kwargs...)
     ax = Axis(gp; axis_attributes(ta; add_title)..., axis...)
     plots = specplot!(ax, ta; kwargs...)
-    add_colorbar && isspectrogram(ta) && Colorbar(gp[1, 1, Right()], plots; label=clabel(ta))
-    PanelAxesPlots(gp, AxisPlots(ax, plots))
+    add_colorbar && isspectrogram(ta) && Colorbar(gp[1, 1, Right()], plots; label = clabel(ta))
+    return PanelAxesPlots(gp, AxisPlots(ax, plots))
 end
 
 """
 Plot heatmap of a time series on the same axis
 """
-function specplot!(ax::Axis, ta; labels=labels(ta), verbose=true, kwargs...)
+function specplot!(ax::Axis, ta; labels = labels(ta), verbose = true, kwargs...)
     ta = resample(ta; verbose)
-    heatmap!(ax, makie_x(ta), spectrogram_y_values(ta), ta.data; heatmap_attributes(ta; kwargs...)...)
+    return heatmap!(ax, makie_x(ta), spectrogram_y_values(ta), ta.data; heatmap_attributes(ta; kwargs...)...)
 end
+
+MakieCore.convert_arguments(T::Type{<:SpecPlot}, A::AbstractArray{<:Number}, args...) =
+    plot2spec(T, A, args...)
 
 function plot2spec(::Type{<:SpecPlot}, da; kwargs...)
     x = makie_x(da)
     y = spectrogram_y_values(da)
     attributes = heatmap_attributes(da; kwargs...)
-    S.Heatmap(x, y, parent(da); attributes...)
+    return S.Heatmap(x, y, parent(da); attributes...)
 end
