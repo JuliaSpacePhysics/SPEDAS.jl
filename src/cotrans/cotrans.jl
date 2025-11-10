3b7466c2108eb237cdacfd3cbfdc62418fca1b81
Beforerr <zzj956959688@gmail.com>
2025-11-10 12:10:33 -0800

    docs: move Radiation Belt Modeling into IRBEM.jl

--- src/cotrans/cotrans.jl
+++ src/cotrans/cotrans.jl
@@ -9,21 +9,29 @@ include("fac.jl")
 using GeoCotrans: coord_maps
 
 """
-    cotrans(A, in, out)
+    cotrans(A, in, out; backend=:auto)
     cotrans(A, out; in=get_coord(A))
 
 Transform the data to the `out` coordinate system from the `in` coordinate system.
 
-This function automatically choose between Julia's [`GeoCotrans`](https://juliaspacephysics.github.io/GeoCotrans.jl) (if available) and Fortran's [`IRBEM`](https://juliaspacephysics.github.io/IRBEM.jl) implementation.
+If `backend` is set to `:auto` (default), this function automatically chooses between Julia's [`GeoCotrans`](https://juliaspacephysics.github.io/GeoCotrans.jl) (if available) and Fortran's [`IRBEM`](https://juliaspacephysics.github.io/IRBEM.jl) implementation. Otherwise, it uses the specified backend.
 
 References:
 
-  - [IRBEM-LIB](https://prbem.github.io/IRBEM/): compute magnetic coordinates and perform coordinate conversions ([Documentation](https://prbem.github.io/IRBEM/api/coordinates_transformations.html), [IRBEM.jl](https://github.com/Beforerr/IRBEM.jl))
+  - [IRBEM-LIB](https://prbem.github.io/IRBEM/): compute magnetic coordinates and perform coordinate conversions ([Documentation](https://prbem.github.io/IRBEM/api/coordinates_transformations.html), [IRBEM.jl](https://github.com/JuliaSpacePhysics/IRBEM.jl))
   - [SPEDAS Cotrans](https://spedas.org/wiki/index.php?title=Cotrans)
 """
-function cotrans(A, in, out)
+function cotrans(A, in, out; backend = :auto)
+    backend = Symbol(backend) # handle Module
+    @assert backend in (:auto, :GeoCotrans, :IRBEM) "backend must be :auto, :GeoCotrans, or :IRBEM"
     key = (Symbol(lowercase(in)), Symbol(lowercase(out)))
-    Ac = haskey(coord_maps, key) ? coord_maps[key](A) : irbem_cotrans(A, in, out)
+    Ac = if backend == :auto
+        haskey(coord_maps, key) ? coord_maps[key](A) : irbem_cotrans(A, in, out)
+    elseif backend == :GeoCotrans
+        coord_maps[key](A)
+    else
+        irbem_cotrans(A, in, out)
+    end
     return set_coord(Ac, out)
 end
 
